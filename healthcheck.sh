95e98edb500a205e6feb19915c8e63c7a4eb13c3
fulvio <fulvioduarte@gmail.com>
2026-01-15 18:44:22 -0300

    Ajuste

--- healthcheck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Testa se Laravel responde na porta do container
-if curl -s http://localhost:8080 | grep -q "Laravel"; then
-    echo "Laravel está funcionando ✅"
-else
-    echo "Laravel NÃO está respondendo ❌"
-fi
